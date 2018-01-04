1c25a6bf0996846309bfe0befdf35900ab738002
Leonardo Alves <alexacaalves@gmail.com>
2018-01-04 19:33:02 -0200

    Adicionar modulo de conexao ao firebase

---
+++ scrapper/firebase_entry.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+"""Modulo responsavel por conectar-se ao firebase RealTime Database, e salvar os dados coletados."""
+
+import firebase_admin
+import json 
+
+from firebase_admin import db
+from firebase_admin import credentials
+
+cred = credentials.Certificate("serviceAccountKey.json")
+
+firebase_admin.initialize_app(cred, { 'databaseURL': 'https://medium-toptends.firebaseio.com/' })
+
+ref = db.reference('/')
+
+def salvar_topicos(topics):
+    print('A salvar topicos...')
+    ref.child('topics').set(topics)
+    print('Topicos salvos com sucesso.')
