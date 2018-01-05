7381084c551d9a4d43405ee7c9cea10574d95f6a
Leonardo Alves <leunardosevla@gmail.com>
2018-01-05 18:52:04 -0200

    Refatorar controller da home

--- app/js/home.controller.js
+++ app/js/home.controller.js
@@ -1,24 +1,8 @@
 app.controller('homeController', homeController);
 
-function homeController($scope) {
-    const alturaWc = window.innerHeight - (window.innerHeight * .4);
-    const larguraWc = window.innerWidth - (window.innerWidth * .3);
-    $scope.oi = 'Oi tudo bom?';
-
-    const firebase = document.getFirebaseApp();
-    const db = firebase.database();
-    db.ref('/trends/general').once('value').then(snapshot => {
-        const data = snapshot.val();
-        data.forEach(d => d.size *= 4);
-
-        $scope.wc = {
-            'words': data,
-            'height': alturaWc,
-            'width': larguraWc,
-            'useTransition': true,
-            'rotate': 45
-        };
+function homeController($scope, wcService) {
+    wcService.show('/trends/general', (data) => {
+        $scope.wc = data;
         $scope.$apply();
     });
-
 }
