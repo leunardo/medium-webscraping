ce2996e6709066e73b340f47a7847349971c49ac
Leonardo Alves <alexacaalves@gmail.com>
2018-01-04 22:51:53 -0200

    Atualizar database

--- scrapper/firebase_entry.py
+++ scrapper/firebase_entry.py
@@ -9,7 +9,7 @@ from firebase_admin import credentials
 
 cred = credentials.Certificate("serviceAccountKey.json")
 
-firebase_admin.initialize_app(cred, { 'databaseURL': 'https://medium-toptends.firebaseio.com/' })
+firebase_admin.initialize_app(cred, { 'databaseURL': 'https://medium-toptrends.firebaseio.com/' })
 
 ref = db.reference('/')
 
